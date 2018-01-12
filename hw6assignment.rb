fb3b49cda19dfbc95137f8b6859d79fc3b50f941
drazen41 <drazen.svec@gmail.com>
2018-01-12 15:44:38 +0100

    Tetris - MyPieces

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -6,29 +6,43 @@
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
   # class array holding all the pieces and their rotations
+  
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+                 # rotations([[0, 0], [1, 0], [0, 1],[0,0]]), # short T/L
+                  #rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                # [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                 [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                  [[[0, 0], [-1, 0], [1, 0], [2, 0]]] # long long (only needs two)
+                 #[[0, 0], [0, -1], [0, 1], [0, 2]]], 
+                 #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # fat L  
+                 #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                 #rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                 #rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                 #rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+                   
+  # your enhancements here
   def initialize(point_array, board) 
-    super
+    @all_rotations = point_array
+        @rotation_index = (0..(@all_rotations.size-1)).to_a.sample
+        @color = All_Colors.sample
+        @base_position = [5, 0] # [column, row]
+        @board = board
+        @moved = true
   end
   def self.next_piece (board)
       MyPiece.new(All_My_Pieces.sample, board)
   end
-  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-                 rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-                 [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-                 [[0, 0], [0, -1], [0, 1], [0, 2]]],
-                 rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-                 rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-                 rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-                 rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
-  # your enhancements here
-
 end
 
 class MyBoard < Board
   # your enhancements here
-  def initialize(board)
-    @current_block = MyPiece.next_piece(self)
-    super
+  def initialize(game)
+   
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+        @current_block = MyPiece.next_piece(self)
+        @score = 0
+        @game = game
+        @delay = 500
   end
   
   def rotate_180
@@ -36,6 +50,10 @@ class MyBoard < Board
         @current_block.move(0, 0, 2)
       end
       draw
+  end
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
   end
 end
 
